50bcfc78c2f6ad6b0e6d05f9005b08df4ab03d0c
Paolo Dionesalvi <dionesalvip@gmail.com>
2024-11-21 09:56:32 +0100

    Refactor server.h and reader.c for Lab08

--- Laboratorio/Lab08/writer_reader/reader.c
+++ Laboratorio/Lab08/writer_reader/reader.c
@@ -12,33 +12,6 @@
 
 #include "server.h"
 
-int initSemAvailable(int semId, int semNum) {
-    union semun arg;
-    arg.val = 1;
-    return semctl(semId, semNum, SETVAL, arg);
-}
-int initSemInUse(int semId, int semNum) {
-    union semun arg;
-    arg.val = 0;
-    return semctl(semId, semNum, SETVAL, arg);
-}
-
-int reserveSem(int semId, int semNum) {
-    struct sembuf sops;
-    sops.sem_num = semNum;
-    sops.sem_op = -1;
-    sops.sem_flg = 0;
-    return semop(semId, &sops, 1);
-}
-
-int releaseSem(int semId, int semNum) {
-    struct sembuf sops;
-    sops.sem_num = semNum;
-    sops.sem_op = 1;
-    sops.sem_flg = 0;
-    return semop(semId, &sops, 1);
-}
-
 int main(int argc, char *argv[]){
     int semid;
     union semun arg;
--- Laboratorio/Lab08/writer_reader/server.h
+++ Laboratorio/Lab08/writer_reader/server.h
@@ -5,3 +5,31 @@ struct mymsgg {
     long mtype;
     char mtext[100];
 };
+
+
+int initSemAvailable(int semId, int semNum) {
+    union semun arg;
+    arg.val = 1;
+    return semctl(semId, semNum, SETVAL, arg);
+}
+int initSemInUse(int semId, int semNum) {
+    union semun arg;
+    arg.val = 0;
+    return semctl(semId, semNum, SETVAL, arg);
+}
+
+int reserveSem(int semId, int semNum) {
+    struct sembuf sops;
+    sops.sem_num = semNum;
+    sops.sem_op = -1;
+    sops.sem_flg = 0;
+    return semop(semId, &sops, 1);
+}
+
+int releaseSem(int semId, int semNum) {
+    struct sembuf sops;
+    sops.sem_num = semNum;
+    sops.sem_op = 1;
+    sops.sem_flg = 0;
+    return semop(semId, &sops, 1);
+}
